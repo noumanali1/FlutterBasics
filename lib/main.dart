0a12ba60d6ab878bc214ef1a719923c3255dea95
Ch Nouman Ali <76949698+noumanali1@users.noreply.github.com>
2023-09-06 00:07:59 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -57,22 +57,3 @@ void main() {
     ),
   );
 }
-
-// void main() {
-//   runApp(
-//     MyApp()
-//   );
-// }
-// class MyApp extends StatelessWidget {
-//   // const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         backgroundColor: Colors.teal,
-//         body: Container(),
-//       ),
-//     );
-//   }
-// }
